9c24d34de4eb654fbb5fd8e74b378a95af65ec8b
= <=>
2023-11-29 18:44:39 -0300

    try 3081

--- GEOMETRIA/NIVEL-5/3081.js
+++ GEOMETRIA/NIVEL-5/3081.js
@@ -1,80 +1,17 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
-class Vector3 {
-    constructor(x = 0, y = 0, z = 0) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    sub(vector3) {
-        return new Vector3(
-            this.x - vector3.x,
-            this.y - vector3.y,
-            this.z - vector3.z
-        );
-    }
-    add(vector3) {
-        return new Vector3(
-            this.x + vector3.x,
-            this.y + vector3.y,
-            this.z + vector3.z
-        );
-    }
-    mag() {
-        return Math.hypot(this.x, this.y, this.z);
-    }
-    scale(c) {
-        return new Vector3(
-            this.x * c,
-            this.y * c,
-            this.z * c
-        );
-    }
-    norm() {
-        const mag = this.mag();
-        return new Vector3(
-            this.x / mag,
-            this.y / mag,
-            this.z / mag
-        )
-    }
-}
-
-const out = (vector3) => {
-    console.log(
-        `${vector3.x.toFixed(3)} ${vector3.y.toFixed(3)} ${vector3.z.toFixed(3)}`
-    );
-};
-
-const predictNextPositionBasedInFirstAndSecondPositions = (firstPositionVector3, secondPositionVector3) => {
-    const diff = firstPositionVector3.sub(secondPositionVector3);
-    const diffNorm = diff.norm();
-    const diffMag = diff.mag();
-    const projection = secondPositionVector3.add(diffNorm.scale(diffMag));
-    return projection;
-}
-
-
 while (lines.length) {
     const [ax, ay, az, bx, by, bz] = lines.shift().split(' ').map(n => +n);
-    const time = +lines.shift();
+    let time = +lines.shift();
+    const r = Math.hypot(ax, ay, az);
+    const t = Math.PI * r * r * r; // find this value
+    const x = ax * r * Math.cos(t) + bx * r * Math.sin(t);
+    const y = ay * r * Math.cos(t) + by * r * Math.sin(t);
+    const z = az * r * Math.cos(t) + bz * r * Math.sin(t);
 
-    let firstPosition = new Vector3(ax, ay, az);
-    let secondPosition = new Vector3(bx, by, bz);
+    console.log(x, y, z);
 
 
-    if (time <= 1) {
-        console.log(time === 0 ? out(firstPosition) : out(secondPosition));
-        continue;
-    }
-
-    for (let i = 1; i < time; i++) {
-        let aux = secondPosition;
-        secondPosition = predictNextPositionBasedInFirstAndSecondPositions(firstPosition, secondPosition);
-        firstPosition = aux;
-    }
-
-    out(secondPosition);
 }
 
